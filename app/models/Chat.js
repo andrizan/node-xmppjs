a9a37012f35051445a138d080225caeff6003b4f
Riski Andriyanto <riski.andriyanto0111@gmail.com>
2020-11-17 11:46:20 +0700

    edit chart models

--- app/models/Chat.js
+++ app/models/Chat.js
@@ -1,13 +1,19 @@
 const mongoose = require('mongoose');
 
-const chatScheme = new mongoose.Schema({
-  jid: {
-    type: String,
-    required: true
+const chatScheme = new mongoose.Schema(
+  {
+    jid: {
+      type: String,
+      required: true
+    },
+    msg: {
+      type: String,
+      required: true
+    }
   },
-  msg: {
-    type: String,
-    required: true
-  }
-});
+  {
+    strict: true,
+    versionKey: false,
+    timestamps: { createdAt: "createdAt", updatedAt: "updatedAt" },
+  });
 module.exports = mongoose.model("Chat", chatScheme);
